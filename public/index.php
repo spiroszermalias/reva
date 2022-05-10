6d4412f81a249bbd8975e76ae005fc212da07f09
Spiros Zermalias <me@spiroszermalias.com>
2022-05-10 17:34:01 +0300

    Add public index file header DocBlock

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * ReVa is the go-to project to manage your organization employee-vacation
+ *
+ * @version 1.0.0
+ * @link https://github.com/spiroszermalias/reva
+ * @package reva
+ * @author Spiros Zermalias <me@spiroszermalias.com>
+ */
