731dbe53b80955f0850ecbe80336ae82ac370600
Spiros Zermalias <me@spiroszermalias.com>
2022-05-12 14:20:09 +0300

    Add timestamp option as a return type

--- app/functions/helpers.php
+++ app/functions/helpers.php
@@ -1,11 +1,15 @@
 <?php
 
-function now() {
+function now( $format = 'datetime' ) {
+    $time = new stdClass();
+    $datetime = '';
+    $timestamp = '';
+
     /** Perfom call to API */
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://worldtimeapi.org/api/timezone/'.TIMEZONE,
+    CURLOPT_URL => 'http://worldtimeapi.org/api/timezone/'.TIMEZONE,
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
@@ -20,15 +24,39 @@ function now() {
     curl_close($curl);
     
     /** If API call is successful, extract datetime */
-    if ($httpcode === '200') :
+    if ($httpcode === 200) :
         $response_obj = json_decode($response);
         $datetime_unformatted_string = $response_obj->datetime;
 
         $datetime_obj = date_create($datetime_unformatted_string);
-        return date_format($datetime_obj, DATETIME_FORMAT);
+        $datetime = date_format($datetime_obj, DATETIME_FORMAT);
+        
+        $datetime = $datetime;
+        $timestamp_utc_0 = $datetime_obj->getTimestamp();
+        
+        //Retrieve the UTC offset in unix time
+        $target_time_zone = new DateTimeZone( TIMEZONE );
+        $date_time = new DateTime('now', $target_time_zone);
+        $timestamp_utc_offset = $date_time->format('Z');
+        
+        //Timestamp with UTC offset
+        $timestamp = $timestamp_utc_0 + $timestamp_utc_offset;
+        
+        return ($format === 'timestamp')? $timestamp : $datetime;
     endif;
     
     /** If API call wasn't successful, fall-back to system time */
-    $date = new DateTime("now", new DateTimeZone( TIMEZONE ) );
-    return $date->format( DATETIME_FORMAT );
+    $datetime_obj = new DateTime("now", new DateTimeZone( TIMEZONE ) );
+    $datetime = $datetime_obj->format( DATETIME_FORMAT );
+    $timestamp_utc_0 = $datetime_obj->getTimestamp();
+
+    //Retrieve the UTC offset in unix time
+    $target_time_zone = new DateTimeZone( TIMEZONE );
+    $date_time = new DateTime('now', $target_time_zone);
+    $timestamp_utc_offset = $date_time->format('Z');
+    
+    //Timestamp with UTC offset
+    $timestamp = $timestamp_utc_0 + $timestamp_utc_offset;
+    
+    return ($format === 'timestamp')? $timestamp : $datetime;
 }
