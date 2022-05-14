a708a633411425fc4495b0d933d87c52c5e0a363
Spiros Zermalias <spiroszermalias@gmail.com>
2022-05-15 01:39:30 +0300

    Correct SQL Syntax error

--- app/core/db.php
+++ app/core/db.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Core\Db class (Singleton) performs checks and performs actions relating to the database entity structure.
+ * Core\Db class performs checks and performs actions relating to the database entity structure.
  * 
  * @link https://github.com/spiroszermalias/reva
  * @package reva
@@ -12,44 +12,40 @@ namespace Core;
 
 class Db
 {
-    /** Hold the class instance. */
-    private static $instance = NULL;
 
     /**
      * $tables contains the required tables.
      * Key should be table name and value the SQL syntax describing the attribute
      */
     private static $tables = array(
-        'users' =>
-        '`user_id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-        `username` VARCHAR( 255 ) NOT NULL ,
-        `first_name` tinytext COLLATE,
-        `last_name` tinytext COLLATE,
-        `user_email` VARCHAR( 60 ) NOT NULL ,
-        `role` tinytext COLLATE,
-        `user_pass` VARCHAR( 255 ) NOT NULL ,
-         UNIQUE (`user_name`),
-         UNIQUE (`user_email`)'
-        ,
+        'users' =>"
+            `user_id` int(11) NOT NULL,
+            `user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+            `first_name` tinytext COLLATE utf8mb4_unicode_ci,
+            `last_name` tinytext COLLATE utf8mb4_unicode_ci,
+            `role` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+            `user_pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+        ",
         'token_auth' => "
-        `id` int(11) NOT NULL PRIMARY KEY,
-        `username` varchar(255) NOT NULL,
-        `password_hash` varchar(255) NOT NULL,
-        `selector_hash` varchar(255) NOT NULL,
-        `is_expired` int(11) NOT NULL DEFAULT '0',
-        `expiry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
-        ,
+            `id` int(11) NOT NULL PRIMARY KEY,
+            `username` varchar(255) NOT NULL,
+            `password_hash` varchar(255) NOT NULL,
+            `selector_hash` varchar(255) NOT NULL,
+            `is_expired` int(11) NOT NULL DEFAULT '0',
+            `expiry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        ",
         'applications' => "
-        `id` int(11) NOT NULL,
-        `user_id` int(11) NOT NULL,
-        `submit_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        `from_datetime` datetime NOT NULL,
-        `to_datetime` datetime NOT NULL,
-        `reason` text COLLATE utf8mb4_unicode_ci NOT NULL,
-        `status` tinytext COLLATE utf8mb4_unicode_ci NOT NULL"
+            `id` int(11) NOT NULL,
+            `user_id` int(11) NOT NULL,
+            `submit_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+            `from_datetime` datetime NOT NULL,
+            `to_datetime` datetime NOT NULL,
+            `reason` text COLLATE utf8mb4_unicode_ci NOT NULL,
+            `status` tinytext COLLATE utf8mb4_unicode_ci NOT NULL
+        "
     );
 
-    private function __construct() {
+    public function __construct() {
         $this->check_db();
     }
 
@@ -71,19 +67,6 @@ class Db
                 \DB::query( "CREATE TABLE `{$table_name}` ( {$table_fields} ) ENGINE = InnoDB;" );
             endforeach;
         endif;
-    }
-
-    /**
-     * Always returns the single, same instance of the class
-     *
-     * @return Db class object
-     */
-    public static function getInstance() : self {
-        if (self::$instance == null) :
-            self::$instance = new Db;
-        endif;
-
-        return self::$instance;
     }
 
 //end class
