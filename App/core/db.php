074521fce753d823a6aec13cfef02b78bb7a260b
Spiros Zermalias <me@spiroszermalias.com>
2022-05-15 02:42:48 +0300

    Minor fixes

--- App/core/db.php
+++ App/core/db.php
@@ -19,33 +19,33 @@ class Db
      */
     private static $tables = array(
         'users' =>"
-            `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            `user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-            `first_name` tinytext COLLATE utf8mb4_unicode_ci,
-            `last_name` tinytext COLLATE utf8mb4_unicode_ci,
-            `role` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
-            `user_pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-            UNIQUE (`user_email`),
-            INDEX (`user_email`)
-        ",
-        'token_auth' => "
-            `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            `user_email` varchar(255) NOT NULL,
-            `password_hash` varchar(255) NOT NULL,
-            `selector_hash` varchar(255) NOT NULL,
-            `is_expired` int(11) NOT NULL DEFAULT '0',
-            `expiry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-        ",
-        'applications' => "
-            `id` int(11) NOT NULL AUTO_INCREMENT,
-            `user_id` int(11) NOT NULL,
-            `submit_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            `from_datetime` datetime NOT NULL,
-            `to_datetime` datetime NOT NULL,
-            `reason` text COLLATE utf8mb4_unicode_ci NOT NULL,
-            `status` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
-            INDEX ('user_id')
-        "
+        `user_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+        `first_name` tinytext COLLATE utf8mb4_unicode_ci,
+        `last_name` tinytext COLLATE utf8mb4_unicode_ci,
+        `role` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+        `user_pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+        UNIQUE (`user_email`),
+        INDEX (`user_email`)
+    ",
+    'token_auth' => "
+        `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_email` varchar(255) NOT NULL,
+        `password_hash` varchar(255) NOT NULL,
+        `selector_hash` varchar(255) NOT NULL,
+        `is_expired` int(11) NOT NULL DEFAULT '0',
+        `expiry_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    ",
+    'applications' => "
+        `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        `user_id` int(11) NOT NULL,
+        `submit_datetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        `from_datetime` datetime NOT NULL,
+        `to_datetime` datetime NOT NULL,
+        `reason` text COLLATE utf8mb4_unicode_ci NOT NULL,
+        `status` tinytext COLLATE utf8mb4_unicode_ci NOT NULL,
+        INDEX (`user_id`)
+    "
     );
 
     public function __construct() {
